20384fa76a879fdc5932cbdc2f061b2f232f35ab
Agamal88 <132816767+Agamal88@users.noreply.github.com>
2024-10-25 20:03:33 +0300

    Added the Login Page

---
+++ frontend/src/componets/LoginForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./RegisterationForm.css";
+import { Link } from "react-router-dom";
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+    setFormData({ email: "", password: "" });
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+        <p>
+          Don't have an account ? <Link to="/">Register</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
