29e5d7ed63ca1d90c418d8ca7aeb249c0342a02c
Shahd Ahmed Mohamed kamal <147663642+ShahdAhmed2003@users.noreply.github.com>
2024-12-24 18:37:12 +0200

    Update CourierAssignedOrders.js

--- frontend/src/componets/CourierAssignedOrders.js
+++ frontend/src/componets/CourierAssignedOrders.js
@@ -1,138 +1,137 @@
 import React, { useState, useEffect } from 'react';
-import { useParams,useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import "../assets/styless.css";
 import { decodeToken } from "../utils/auth";
 import LogoutButton from './Log-out';
 import Header from './Header';
-const OrdersPage = ({onLogout}) => {
-  
+
+const OrdersPage = ({ onLogout }) => {
+
   const [orders, setOrders] = useState([]);
-   const token=localStorage.getItem("token");
-   const [error, setError] = useState(null);
-   const [message, setMessage] = useState(""); 
+  const token = localStorage.getItem("token");
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState("");
 
-   let userID=null;
-    if(token){
-      const decodedToken=decodeToken(token);
-      userID=decodedToken.userID;
+  let userID = null;
+  if (token) {
+    const decodedToken = decodeToken(token);
+    userID = decodedToken.userID;
   }
-  const navigate = useNavigate(); 
+
+  const navigate = useNavigate();
+
   useEffect(() => {
-
     const fetchOrdersForCourier = async () => {
-      try{
+      try {
+        const response = await fetch("https://backend-shahdahmed851-dev.apps.rm2.thpm.p1.openshiftapps.com/api/orders/assigned_orders", {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
 
-      const response = await fetch("http://localhost:8080/api/orders/assigned_orders",
-      {method:'GET',
-      headers:{
-        'Authorization':`Bearer ${token}`
+        if (!response.ok) {
+          throw new Error('Failed to fetch orders');
+        }
+
+        const fetchedOrders = await response.json();
+        console.log(fetchedOrders);
+        if (fetchedOrders.message) {
+          setMessage(fetchedOrders.message);
+          setOrders([]);
+        } else {
+          setOrders(fetchedOrders);
+          setMessage("");
+        }
+
+      } catch (err) {
+        setError(err.message);
       }
-      });
-      if (!response.ok) {
-        throw new Error('Failed to fetch orders');
-      }
-      const fetchedOrders = await response.json();
-      console.log(fetchedOrders);
-      if(fetchedOrders.message)
-      {
-        setMessage(fetchedOrders.message);
-        setOrders([]);
-      }
-      else{
-      setOrders(fetchedOrders);
-      setMessage(""); 
-      }
-      
-    } catch(err)
-    {
-      setError(err.message);
     };
+    fetchOrdersForCourier();
+  }, [token]);
 
-  };
-  fetchOrdersForCourier();
-},[token]);
-  
- const AcceptOrder = async (orderId) => {
-        
-            const response = await fetch(`http://localhost:8080/api/orders/verify?orderId=${orderId}`, {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-
-            if (response.ok) {
-                alert("Order Accepted successfully");
-                
-            } else {
-                const errorData = await response.text();
-                alert("Failed to accept order: " + errorData);
-            }
-            window.location.reload();
-    };
-    const DeclineOrder=async(orderId)=>{
-      const response=await fetch(`http://localhost:8080/api/orders/decline/${orderId}`,{
-       method:'DELETE',
-       headers: {
+  const AcceptOrder = async (orderId) => {
+    const response = await fetch(`https://backend-shahdahmed851-dev.apps.rm2.thpm.p1.openshiftapps.com/api/orders/verify?orderId=${orderId}`, {
+      method: 'POST',
+      headers: {
         'Authorization': `Bearer ${token}`
-    }
+      }
+    });
 
-      })
-      if (response.ok) {
-        alert("Order Declined");
-        
+    if (response.ok) {
+      alert("Order Accepted successfully");
+
     } else {
-        const errorData = await response.text();
-        alert("Failed to decline order: " + errorData);
+      const errorData = await response.text();
+      alert("Failed to accept order: " + errorData);
     }
     window.location.reload();
-    };
+  };
+
+  const DeclineOrder = async (orderId) => {
+    const response = await fetch(`https://backend-shahdahmed851-dev.apps.rm2.thpm.p1.openshiftapps.com/api/orders/decline/${orderId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+
+    if (response.ok) {
+      alert("Order Declined");
+
+    } else {
+      const errorData = await response.text();
+      alert("Failed to decline order: " + errorData);
+    }
+    window.location.reload();
+  };
+
   return (
     <div>
       <header className="header">
-          <div className="logo">
-              <Link to={"/"} className="login-button">Bosta</Link>
-          </div>
-          <div><LogoutButton onLogout={onLogout} /> </div>
-
+        <div className="logo">
+          <Link to={"/"} className="login-button">Bosta</Link>
+        </div>
+        <div><LogoutButton onLogout={onLogout} /> </div>
       </header>
-      <br/>
-      <h1>Orders for Courier:{userID} </h1>
+      <br />
+      <h1>Orders for Courier: {userID}</h1>
       {message ? (
         <p>{message}</p>
       ) : (
-      <table>
-        <thead>
-          <tr>
-            <th>Order ID</th>
-            <th>Pick-up location</th>
-            <th>Drop-off location</th>
-            <th>Status</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map(order => (
-            <tr key={order.ID}>
-              <td>{order.ID}</td>
-              <td>{order.pickup_location.street_address}</td>
-              <td>{order.drop_off_location.street_address}</td>
-              <td>{order.status}</td>
-              <td>
-                <div className="button-group">
-                  {order.status === "pending" && (
-                    <>
-                      <button onClick={() => AcceptOrder(order.ID)}>Accept</button>
-                      <button className="refuse-button" onClick={() => DeclineOrder(order.ID)}>Refuse</button>
-                    </>
-                  )}
-                  {order.status === "accepted" && <span>Already Accepted</span>}
-                </div>
-              </td>
+        <table>
+          <thead>
+            <tr>
+              <th>Order ID</th>
+              <th>Pick-up location</th>
+              <th>Drop-off location</th>
+              <th>Status</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {orders.map(order => (
+              <tr key={order.ID}>
+                <td>{order.ID}</td>
+                <td>{order.pickup_location.street_address}</td>
+                <td>{order.drop_off_location.street_address}</td>
+                <td>{order.status}</td>
+                <td>
+                  <div className="button-group">
+                    {order.status === "pending" && (
+                      <>
+                        <button onClick={() => AcceptOrder(order.ID)}>Accept</button>
+                        <button className="refuse-button" onClick={() => DeclineOrder(order.ID)}>Refuse</button>
+                      </>
+                    )}
+                    {order.status === "accepted" && <span>Already Accepted</span>}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
       <button onClick={() => navigate('/courier/manageOrders')}>Update Orders</button>
     </div>
